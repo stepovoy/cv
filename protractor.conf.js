7a042dc316ee8744c34ce17cccdaeb038f540662
stepovoy <arthurstepovoy@gmail.com>
2018-10-29 16:26:03 +0200

    config files rollback

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://stepovoy.herokuapp.com/',
+  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
